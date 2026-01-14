edca55d4e12a7baad5f7cd6f373b9d9d53b73f0e
samtheman19 <makin.samuel@gmail.com>
2026-01-14 22:07:47 +0000

    Bump cache version and update asset list

--- sw.js
+++ sw.js
@@ -1,38 +1,50 @@
 /* Service Worker – cache version bump so updates show on Home Screen */
-const CACHE = "treadmill810-cache-v6";
+const CACHE = "treadmill810-cache-v7";
 const ASSETS = [
   "./",
   "./index.html",
   "./app.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./sw.js"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches
+      .open(CACHE)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
-    ).then(() => self.clients.claim())
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+      )
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
   const req = event.request;
+
   event.respondWith(
     caches.match(req).then((cached) => {
-      return cached || fetch(req).then((res) => {
-        // Cache new GET requests
-        if (req.method === "GET" && res.ok) {
-          const copy = res.clone();
-          caches.open(CACHE).then((cache) => cache.put(req, copy)).catch(()=>{});
-        }
-        return res;
-      }).catch(() => cached);
+      return (
+        cached ||
+        fetch(req)
+          .then((res) => {
+            if (req.method === "GET" && res.ok) {
+              const copy = res.clone();
+              caches.open(CACHE).then((cache) => cache.put(req, copy)).catch(() => {});
+            }
+            return res;
+          })
+          .catch(() => cached)
+      );
     })
   );
 });
